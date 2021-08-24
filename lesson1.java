5a8bb374128f9ad4d678cdb710828b03d3887c52
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-08-24 09:08:23 -0500

    Update lesson1.java

--- lesson1.java
+++ lesson1.java
@@ -5,6 +5,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This will be printed");
+        byte zero = 0;
+        String output = "W" + zero + "w";
+        System.out.println(output);
     }
 }
