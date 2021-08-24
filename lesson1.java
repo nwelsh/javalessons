536dc22c3ba72d5f6eed30ab70e9c8be90ef4c40
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-08-24 09:07:14 -0500

    Update lesson1.java

--- lesson1.java
+++ lesson1.java
@@ -1,3 +1,10 @@
 // Lesson1: https://www.learnjavaonline.org/
 // Date: 8/24/21
 // Author: Nicole 
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This will be printed");
+    }
+}
