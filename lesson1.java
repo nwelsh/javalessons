d0344a7e6f4fded3706349e835e484c3dc3459e7
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-08-25 09:06:02 -0500

    Update lesson1.java

--- lesson1.java
+++ lesson1.java
@@ -10,3 +10,21 @@ public class Main {
         System.out.println(output);
     }
 }
+
+//exercise 2 
+public class Main {
+    public static void main(String[] args) {
+        String a = new String("Wow");
+        String b = a;
+        String c = a + "!";
+        String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+            System.out.println("Success!");
+        }
+    }
+}
