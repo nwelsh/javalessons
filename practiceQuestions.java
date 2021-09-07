f28a1165b5cf4c38dc5281fa4b7fef44b1a6b0bc
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-09-07 08:36:04 -0500

    Update practiceQuestions.java

--- practiceQuestions.java
+++ practiceQuestions.java
@@ -4,8 +4,13 @@
 // Question 1: what is the difference between instance variables and local variables 
 // instance variables are accessible by all methods in the class 
 class Athlete {
-public String athleteName;
+public String athleteName; // these are instance 
 public double athleteSpeed;
 public int athleteAge;
 }
 //local varibales are present within a block, function, or constructor and can only be accessed inside that
+public void athlete() {
+String athleteName; // these are local 
+double athleteSpeed;
+int athleteAge;
+}
